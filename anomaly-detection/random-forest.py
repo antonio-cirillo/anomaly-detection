f8800b53c5948d01fc0723a63293a390d7a499c6
Antonio CIrillo <dev.cirillo@gmail.com>
2021-11-26 18:06:05 +0100

    added comments

--- anomaly-detection/random-forest.py
+++ anomaly-detection/random-forest.py
@@ -16,7 +16,7 @@ dataFrame = pd.read_csv(
 
 # Salvo le features del dataset
 FEATURES = dataFrame.columns.values
-FEATURES = np.append(FEATURES[0:2], FEATURES[5:int(len(FEATURES) - 2)])
+FEATURES = np.append(FEATURES[0:2], FEATURES[5:-2])
 N_FEATURES = int(len(FEATURES))
 
 # Inizializzo la matrice X
@@ -29,24 +29,27 @@ y = dataFrame.iloc[:, N_FEATURES + 1].values
 # Standardizzo i valori delle features
 X = StandardScaler().fit_transform(X)
 
+# Divido il dataset in 70% dataset di traning e 30% dataset di test
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
+# Creiamo un'istanza di RandomForest composta da 100 alberi di decisione
 from sklearn.ensemble import RandomForestClassifier
-
 clf = RandomForestClassifier(n_estimators = 100)
 
+# Addestriamo l'algoritmo sul dataset di training
 clf.fit(X_train, y_train)
+
+# Eseguiamo l'algoritmo sul dataset di testing
 y_pred = clf.predict(X_test)
 
-"""
+# Confronto il vettore target con il vettore delle predizioni
 from sklearn import metrics
-print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
-"""
+print("Accuracy: " + str(metrics.accuracy_score(y_test, y_pred)))
 
-feature_imp = pd.Series(clf.feature_importances_, 
-    index = FEATURES).sort_values(ascending = False)
-
-print(feature_imp)
-
+# Stampo il tempo di esecuzione dell'algoritmo
 print('\nTime elapsed: ' + str(round(time.time() - start_time, 2)) + ' second(s)')
+
+# Visualizzo le prime 10 features più importanti del dataset
+from util.util import plotFeatureImportances
+plotFeatureImportances(clf, FEATURES, 'image/feature-importances.png', 10)
