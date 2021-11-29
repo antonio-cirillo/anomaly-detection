ab8ef56258372f372fe328b002c686e6b7d81e84
Antonio CIrillo <dev.cirillo@gmail.com>
2021-11-29 17:45:07 +0100

    update pca method

--- anomaly-detection/pca.py
+++ anomaly-detection/pca.py
@@ -26,8 +26,15 @@ targetDataFrame = dataFrame.iloc[:, N_FEATURES + 1]
 # Standardizzo i valori delle features
 X = StandardScaler().fit_transform(X)
 
+"""
+scalerDataFrame = pd.concat([
+    pd.DataFrame(X, columns = np.append([0, 1], 
+        np.arange(5, N_FEATURES))), targetDataFrame], axis = 1)
+scalerDataFrame.to_csv('dataset/UNSW_NB15_testing-set_SCALER.csv')
+"""
+
 # Valutiamo la PCA prendendo in considerazione le prime n componenti
-N_COMPONENTS_TO_EVALUATE = 15
+N_COMPONENTS_TO_EVALUATE = 10
 pca = PCA(n_components = N_COMPONENTS_TO_EVALUATE)
 principalComponents = pca.fit_transform(X)
 
@@ -44,30 +51,11 @@ plt.ylabel('% Varianza')
 
 plt.show()
 
-"""
-pca = PCA(n_components = 2)
-principalComponents = pca.fit_transform(X)
-
-
 principalDataFrame = pd.DataFrame(principalComponents,
-    columns = ['principal component 1', 'principal component 2'])
+    columns = range(N_COMPONENTS_TO_EVALUATE))
 finalDataFrame = pd.concat([principalDataFrame, targetDataFrame], axis = 1)
 
-normalDataFrame = finalDataFrame[finalDataFrame['label'] == 0]
-maliciousDataFrame = finalDataFrame[finalDataFrame['label'] == 1]
+# normalDataFrame = finalDataFrame[finalDataFrame['label'] == 0]
+# maliciousDataFrame = finalDataFrame[finalDataFrame['label'] == 1]
 
-plt.scatter(normalDataFrame.iloc[:, 0], normalDataFrame.iloc[:, 1],
-    color = 'blue', marker = 'x', label = 'normal traffic')
-
-plt.scatter(maliciousDataFrame.iloc[:, 0], maliciousDataFrame.iloc[:, 1],
-    color = 'red', marker = 'o', label = 'malicious traffic')
-
-plt.xlabel('principal component 1')
-plt.ylabel('principal compoentn 2')
-plt.legend(loc = 'upper left')
-
-plt.show()
-
-
-print(pca.explained_variance_ratio_.cumsum())
-"""
+finalDataFrame.to_csv('dataset/UNSW_NB15_testing-set_PCA.csv')
