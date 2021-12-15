50ca70a32c675dbec405b034a42f0bc86424e413
Antonio CIrillo <dev.cirillo@gmail.com>
2021-12-15 23:38:25 +0100

    added two method

--- anomaly-detection-binary/util/util.py
+++ anomaly-detection-binary/util/util.py
@@ -1,200 +1,66 @@
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import numpy as np
 
-class Sample:
+def measureAccuracy(labels = [], y_pred = None, y_test = None):
+    n_labels = int(len(labels))
+
+    # Inizializzo l'array di misurazioni
+    measures = []
+    array = []
+    for j in range(n_labels):
+        array.append(0)
+    for i in range(n_labels):
+        measures.append(array.copy())
+
+    n_entries = int(len(y_test))
+    for i in range(n_entries):
+        label_expect = y_test[i]
+        label_predict = y_pred[i]
+        measures[label_expect][label_predict] += 1
+        array[label_predict] += 1
+
+    df = pd.DataFrame(data = measures,
+        index = labels,
+        columns = labels)
+
+    print(df)
+
+    df_copy = df.copy()
+    df = df.apply(lambda x: round(x / x.sum(), 2), axis=1)
+
+    df_normal = df_copy['Normal'].values
+    df_normal = df_normal[1:]
     
-    def __init__(self, listOfCsv):
-        
-        # Memorizzo i tipi di attacchi
-        self.attack_cat_list_ = ['Backdoors', 'Analysis', 'Fuzzers', 
-            'Shellcode', 'Reconnaissance', 'Exploits', 'DoS', 'Worms', 'Generic']
+    np.fill_diagonal(df_copy.values, 0)
 
-        # Memorizzo tutti i sotto dataframe
-        self.df_list_ = []
+    df_copy = df_copy.iloc[1:, :]
+    df_copy = df_copy.iloc[:, 1:]
+    df_anomarl = df_copy.sum(axis = 1).values
 
-        # Leggo il primo dataframe
-        df = pd.read_csv(listOfCsv[0], encoding = 'utf-8')
-        # Trasformo i nomi delle colonne in lettere minuscole
-        df.columns = df.columns.str.lower()
-        # Elimino tutti gli spazi bianchi prima e dopo l'etichetta
-        df['attack_cat'] = df['attack_cat'].str.strip()
-        
-        # Aggiungo come primo dataframe quello relativo al flusso di rete normale
-        self.df_list_.append(df.loc[df['label'] == 0])
-        
-        # Estraggo il sotto dataframe relativo al flusso di rete anomalo
-        df_malicious = df.loc[df['label'] == 1]
+    index = ['False Negative', 'Error in attack labeling']
 
-        # Sostituisco il valore Backdoor con Backdoors in quanto gli attacchi Backdoors
-        # vengono etichettati con queste due stringhe diverse
-        df_malicious['attack_cat'].replace(to_replace = 'Backdoor', value = 'Backdoors')
+    df_copy = pd.DataFrame(data = [df_normal, df_anomarl],
+        index = index,
+        columns = labels[1:])
+    df_copy = df_copy.T
 
-        # Aggiungo alla lista dei dataframe tutti i sotto daframe relativi ad ogni tipologia di attacco
-        for attack in self.attack_cat_list_:
-            self.df_list_.append(df_malicious.loc[df_malicious['attack_cat'] == attack])
+    df_copy = df_copy.apply(lambda x: round(x / x.sum(), 2), axis=1)
 
-        if int(len(listOfCsv)) > 1:
-            # Ripeto questa operazione per ogni altro file csv
-            for index_csv in np.arange(1, int(len(listOfCsv))):
-                df = pd.read_csv(listOfCsv[index_csv], encoding = 'utf-8')
-                df.columns = df.columns.str.lower()
-                df['attack_cat'] = df['attack_cat'].str.strip()
-                self.df_list_[0] = self.df_list_[0].append(df.loc[df['label'] == 0])
-                df_malicious = df.loc[df['label'] == 1]
-                df_malicious['attack_cat'].replace(to_replace = 'Backdoor', value = 'Backdoors')
-                attack_index = 1
-                for attack in self.attack_cat_list_:
-                    self.df_list_[attack_index] = self.df_list_[attack_index].append(
-                        df_malicious.loc[df_malicious['attack_cat'] == attack])
-                    attack_index += 1
-        
-        # Ordino i dataframe da quello con meno entry a quello con più entry
-        self.df_list_.sort(key = lambda d: d.shape[0])
+    colors = ['#1D2F6F', '#8390FA']
+    plot_stackedbar_p(df_copy, index, colors, 'Plot of errors', '')
 
-        # Riordino e aggiungo l'etichetta Normal all'array self.attack_cat_list_
-        self.attack_cat_list_ = []
-        for df in self.df_list_:
-            label = df['attack_cat'].values[0]
-            if isinstance(label, float):
-                self.attack_cat_list_.append('Normal')
-            else:
-                self.attack_cat_list_.append(label)
+    return df
 
-        # Creo un unico dataframe
-        self.sampleDf_ = pd.DataFrame(columns = self.df_list_[0].columns.values)
-        for df in self.df_list_:
-            self.sampleDf_ = self.sampleDf_.append(df)
-
-        # Aggiorno lo stato del dataframe
-        self.update_status_()
-
-        # Salvo il numero di entry del dataframe più piccolo
-        self.minum_ = self.count_attack_cat_list[0]
-
-    def createUniformDataFrame(self, path = None):
-        
-        # Inizializzo il dataframe
-        sampleDf = pd.DataFrame(columns = self.df_list_[0].columns.values)
-        
-        for i in range(int(len(self.df_list_))):
-            df = self.df_list_[i].copy()
-            # Le etichette corrispondono agli indici dell'etichetta all'interno dell'array
-            # self.attack_cat_list_ incrementato di 1 [1, ..., n]
-            # n = |self.attack_cat_list_|
-            df['label'] = df['label'].map({1: (i + 1)})
-            # Estraggo dal dataframe i-esimo m entry dove m è il numero di entry del dataframe più piccolo
-            sampleDf = sampleDf.append(df.sample(n = self.minum_, random_state = 42))
-
-        # Inserisco il valore 0 nelle celle non inizializzate
-        cols = sampleDf.columns
-        sampleDf.loc[:, cols] = sampleDf.loc[:, cols].replace(np.nan, 0)
-
-        # Esporto il dataframe in un file csv
-        if path is not None:
-            sampleDf.to_csv(path)
-
-        # Memorizzo il dataframe dopo le operazioni di sampling
-        self.sampleDf_ = sampleDf
-
-        # Aggiorno lo stato del dataframe
-        self.update_status_()
-
-        return self.sampleDf_.copy()
-
-    def createWeightedDataFrame(self, path = None, weight = 1):
-        
-        # Inizializzo il dataframe
-        sampleDf = pd.DataFrame(columns = self.df_list_[0].columns.values)
-        
-        for i in range(int(len(self.df_list_))):
-            df = self.df_list_[i].copy()
-            # Le etichette corrispondono agli indici dell'etichetta all'interno dell'array
-            # self.attack_cat_list_ incrementato di 1 [1, ..., n]
-            # n = |self.attack_cat_list_|
-            df['label'] = df['label'].map({1: (i + 1)})
-            # Estraggo dal dataframe i-esimo m entry dove m è:
-            # il numero di entry del dataframe più piccolo se i = 0
-            # oppure è il weight% più grande rispetto al numero di entry del dataframe (i - 1)-esimo
-            if i > 0:
-                increase = int(self.minum_ * (1 + weight) ** i)
-                if df.shape[0] >= increase:
-                    sampleDf = sampleDf.append(df.sample(n = increase, random_state = 42))
-                else:
-                    sampleDf = sampleDf.append(df)
-            else:
-                sampleDf = sampleDf.append(df.sample(n = self.minum_))
-
-        # Inserisco il valore 0 nelle celle non inizializzate
-        cols = sampleDf.columns
-        sampleDf.loc[:, cols] = sampleDf.loc[:, cols].replace(np.nan, 0)
-
-        # Esporto il dataframe in un file csv
-        if path is not None:
-            sampleDf.to_csv(path)
-
-        # Memorizzo il dataframe dopo le operazioni di sampling
-        self.sampleDf_ = sampleDf
-
-        # Aggiorno lo stato del dataframe
-        self.update_status_()
-
-        return self.sampleDf_.copy()
-
-    def extractSubDataFrame(self, features, path = None):
-
-        sampleDf = None
-        init = False
-
-        for feature in features:
-            try:
-                if not init:
-                    columnValue = self.sampleDf_[feature].values
-                    sampleDf = pd.DataFrame(data = columnValue, columns = [feature])
-                    init = True
-                else:
-                    sampleDf[feature] = self.sampleDf_[feature].values
-            except KeyError:
-                print('Ignore the', feature, 'feature...')
-
-        # Esporto il dataframe in un file csv
-        if path is not None:
-            sampleDf.to_csv(path)
-
-        # Elimino le colonne con le stringhe
-        sampleDf = self.deleteStringColumn(sampleDf)
-
-        return sampleDf
-
-    def deleteStringColumn(self, dataFrame):        
-        cols_to_remove = []
-
-        for col in dataFrame.columns:
-            try:
-                _ = dataFrame[col].astype(float)
-            except ValueError:
-                cols_to_remove.append(col)
-                pass
-
-        df = dataFrame[[col for col in dataFrame.columns if col not in cols_to_remove]]
-        return df
-
-    def printDataFrameStatus(self):
-        ser = pd.Series(data = self.count_attack_cat_list, index = self.attack_cat_list_)
-        print(ser)
-
-    def update_status_(self):
-        # Dichiaro l'array contenente il numero di entry per attacco
-        self.count_attack_cat_list = []
-
-        for attack in self.attack_cat_list_:
-            if attack == 'Normal':
-                df = self.sampleDf_.loc[self.sampleDf_['label'] == 0]
-                self.count_attack_cat_list.append(df.shape[0])
-            else:
-                df = self.sampleDf_.loc[self.sampleDf_['attack_cat'] == attack]
-                self.count_attack_cat_list.append(df.shape[0])
+def displayHeatMap(metrics, name_file):
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(metrics, annot = True)
+    
+    fig.tight_layout()
+    fig.savefig(name_file, dpi = 100)
+    plt.close()
 
 def deleteStringColumn(df):
     cols_to_remove = []
@@ -229,7 +95,7 @@ def plotFeatureImportances(classifier, features, name_file, n_elements = 5):
     plt.show()
 
 def minimizeDataFrameByFeatureImportances(feature_imp, features, path, min):
-
+    
     feature_imp = pd.Series(feature_imp, 
         index = features).sort_values(ascending = False)
 
@@ -250,3 +116,33 @@ def minimizeDataFrameByFeatureImportances(feature_imp, features, path, min):
     returnDF.to_csv(path.replace('.csv', '_REDUCED.csv'))
 
     return returnDF
+
+def plot_stackedbar_p(df, labels, colors, title, subtitle):
+    fields = df.columns.tolist()
+    
+    fig, ax = plt.subplots(1, figsize=(8, 6))
+
+    left = len(df) * [0]
+    for idx, name in enumerate(fields):
+        plt.barh(df.index, df[name], left = left, color=colors[idx])
+        left = left + df[name]
+
+    plt.title(title, loc='left')
+    plt.text(0, ax.get_yticks()[-1] + 0.75, subtitle)
+
+    plt.legend(labels, bbox_to_anchor=([0.58, 1, 0, 0]), ncol=4, frameon=False)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+    xticks = np.arange(0,1.1,0.1)
+    xlabels = ['{}%'.format(i) for i in np.arange(0,101,10)]
+    plt.xticks(xticks, xlabels)
+
+    plt.ylim(-0.5, ax.get_yticks()[-1] + 0.5)
+    ax.xaxis.grid(color='gray', linestyle='dashed')
+    fig.tight_layout()
+    
+    plt.show()
