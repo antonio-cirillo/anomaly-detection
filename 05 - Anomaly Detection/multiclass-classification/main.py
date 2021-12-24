557876329ffb52907db09623411f893f5645ccbb
Antonio CIrillo <79747869+antonio-cirillo@users.noreply.github.com>
2021-12-24 02:13:58 +0100

    uncomment test

--- 05 - Anomaly Detection/multiclass-classification/main.py
+++ 05 - Anomaly Detection/multiclass-classification/main.py
@@ -4,10 +4,11 @@ from test.test_with_merging import test_with_merging
 from test.test_binary_classification import test_binary_classification
 
 WEIGHTS = [0.25, 0.5, 0.75, 1]
-#test_different_weights(WEIGHTS)
 
-#test_with_augmentation(WEIGHTS)
+test_different_weights(WEIGHTS)
 
-#test_with_merging(WEIGHTS)
+test_with_augmentation(WEIGHTS)
+
+test_with_merging(WEIGHTS)
 
 test_binary_classification()
