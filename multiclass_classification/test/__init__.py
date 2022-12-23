d87ebb054da2ee29d25154ad68fbc6b15d5ea065
Antonio Cirillo <dev.cirillo@gmail.com>
2022-12-23 18:30:45 +0100

    init test package

---
+++ multiclass_classification/test/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+SEPARATOR = '\n------------------------------------\n'
+DETECTION_RATE = 'Detection Rate'
+CONFUSION_MATRIX = 'Confusion Matrix'
+BAR_STACKED = 'Bar Stacked'
+LOGGER = 'logger.log'
+CWD = os.getcwd()
